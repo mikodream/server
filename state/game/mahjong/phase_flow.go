498177a4df31bc2ba9e25ee63006c897f66c61ae
miko <1059665047@qq.com>
2026-01-21 14:46:37 +0800

    fix: Resolve automatic restart prompt & restart issue after game over

--- state/game/mahjong/phase_flow.go
+++ state/game/mahjong/phase_flow.go
@@ -3,7 +3,6 @@ package mahjong
 import (
 	"fmt"
 
-	"github.com/ratel-online/server/consts"
 	"github.com/ratel-online/server/database"
 )
 
@@ -20,29 +19,29 @@ func (g *Mahjong) flowPhase(player *database.Player, game *database.Mahjong) err
 		game.RoundScores[i] = 0
 	}
 
-	// 询问是否继续游戏
-	database.Broadcast(player.RoomID, "是否继续游戏？(y/n)\n")
-
-	// 为了简化，直接重新开始游戏
-	game.GameStatus = consts.GAME_STATUS_DEALING
-	game.WinningInfo = nil
-
-	// 庄家保持不变
-	game.CurrentIndex = game.DealerIndex
-
-	// 重新初始化玩家状态
-	for _, p := range game.Players {
-		p.HandTiles = []database.MahjongTile{}
-		p.ExposedSets = []database.ExposedSet{}
-		p.Actions = []database.Action{}
-	}
-
-	// 开始新一局
-	firstPlayer := game.Players[game.DealerIndex]
-	firstPlayerObj := database.GetPlayer(firstPlayer.ID)
-	if firstPlayerObj != nil {
-		firstPlayer.State <- stateDeal
-	}
+	//// 询问是否继续游戏
+	//database.Broadcast(player.RoomID, "是否继续游戏？(y/n)\n")
+	//
+	//// 为了简化，直接重新开始游戏
+	//game.GameStatus = consts.GAME_STATUS_DEALING
+	//game.WinningInfo = nil
+	//
+	//// 庄家保持不变
+	//game.CurrentIndex = game.DealerIndex
+	//
+	//// 重新初始化玩家状态
+	//for _, p := range game.Players {
+	//	p.HandTiles = []database.MahjongTile{}
+	//	p.ExposedSets = []database.ExposedSet{}
+	//	p.Actions = []database.Action{}
+	//}
+	//
+	//// 开始新一局
+	//firstPlayer := game.Players[game.DealerIndex]
+	//firstPlayerObj := database.GetPlayer(firstPlayer.ID)
+	//if firstPlayerObj != nil {
+	//	firstPlayer.State <- stateDeal
+	//}
 
 	return nil
 }
--- state/game/mahjong/phase_win.go
+++ state/game/mahjong/phase_win.go
@@ -3,7 +3,6 @@ package mahjong
 import (
 	"fmt"
 
-	"github.com/ratel-online/server/consts"
 	"github.com/ratel-online/server/database"
 )
 
@@ -28,33 +27,33 @@ func (g *Mahjong) winPhase(player *database.Player, game *database.Mahjong) erro
 		game.RoundScores[i] = 0
 	}
 
-	// 询问是否继续游戏
-	database.Broadcast(player.RoomID, "是否继续游戏？(y/n)\n")
-
-	// 这里应该收集玩家的选择，为了简化直接继续
-	// 实际实现中需要等待所有玩家确认
-
-	// 重新开始游戏
-	game.GameStatus = consts.GAME_STATUS_DEALING
-	game.WinningInfo = nil
-
-	// 庄家轮转
-	game.DealerIndex = game.NextPlayerIndex(game.DealerIndex)
-	game.CurrentIndex = game.DealerIndex
-
-	// 重新初始化玩家状态
-	for _, p := range game.Players {
-		p.HandTiles = []database.MahjongTile{}
-		p.ExposedSets = []database.ExposedSet{}
-		p.Actions = []database.Action{}
-	}
-
-	// 开始新一局
-	firstPlayer := game.Players[game.DealerIndex]
-	firstPlayerObj := database.GetPlayer(firstPlayer.ID)
-	if firstPlayerObj != nil {
-		firstPlayer.State <- stateDeal
-	}
+	//// 询问是否继续游戏
+	//database.Broadcast(player.RoomID, "是否继续游戏？(y/n)\n")
+	//
+	//// 这里应该收集玩家的选择，为了简化直接继续
+	//// 实际实现中需要等待所有玩家确认
+	//
+	//// 重新开始游戏
+	//game.GameStatus = consts.GAME_STATUS_DEALING
+	//game.WinningInfo = nil
+	//
+	//// 庄家轮转
+	//game.DealerIndex = game.NextPlayerIndex(game.DealerIndex)
+	//game.CurrentIndex = game.DealerIndex
+	//
+	//// 重新初始化玩家状态
+	//for _, p := range game.Players {
+	//	p.HandTiles = []database.MahjongTile{}
+	//	p.ExposedSets = []database.ExposedSet{}
+	//	p.Actions = []database.Action{}
+	//}
+	//
+	//// 开始新一局
+	//firstPlayer := game.Players[game.DealerIndex]
+	//firstPlayerObj := database.GetPlayer(firstPlayer.ID)
+	//if firstPlayerObj != nil {
+	//	firstPlayer.State <- stateDeal
+	//}
 
 	return nil
 }
